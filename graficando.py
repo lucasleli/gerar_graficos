4e2cbb015d401a12ae02b9f9b844367dca221d60
lucasleli <lucas.leli10@gmail.com>
2020-03-09 14:13:42 -0300

    Ajustes e funções da AC02

--- graficando.py
+++ graficando.py
@@ -1,34 +1,62 @@
 import matplotlib.pyplot as plt
 from pathlib import Path
 import numpy as np
-from numpy import sin, cos, log, log10, exp, pi, tan
+from numpy import sin, cos, log, log10, exp, pi, tan, abs, sqrt
 import os.path
 import string
 
-x = np.arange(-2*pi, 2*pi, 0.1)
+x = np.arange(-3*pi, 3*pi, 0.05)
 
-# Todas as funções devem ser passadas nesta lista
-# As funções ainda são passadas de maneira estática, mas irei resolver isso
+# Todas as funções devem ser passadas nesta lista.
+# As funções ainda são passadas de maneira estática, irei resolver isso
 # por meio de um arquivo externo.
 funcoes = [x, -x, x+2, -x+2, x-2, 2*x, -2*x, 2*x+2, -2*x-2, -3*x+2, x**2,
            -x**2, 2*x**2, x**2+1, x**2-1, x**2+3*x-2, -x**2-3*x+2, x**3,
            -x**3, log(x), 1/(sin(x)), exp(x), 1/(cos(x)),
-           tan(x), 1/tan(x), exp(1)**-x, log10(x)]
+           tan(x), 1/tan(x), exp(1)**-x, log10(x), log(x)/log(3),
+           log(x**2), 1/log(x**2), sin(x)-cos(x), cos(x**2+3*x-2),
+           x**4+3*x**2-x+1, 2*cos(2*pi*x+pi), -4*pi*sin(2*pi*x+pi),
+           32*x, 9-x, 1-log(x), 3*x**2+3*x-3, x**3+tan(x), abs(x),
+           9/x, x**-2, x**-3, x**-4, x**-5, sqrt(x), x**2+sqrt(x**3),
+           1/sqrt(x), sqrt(2*pi)*exp(-x**2/2), (4*x**2)**(1/3),
+           9*x**2-log10(x), x*sqrt((9-x)/(9+x)), -x*sqrt((9-x)/(9+x)),
+           9/(x**2+9), cos(exp(x)), exp(cos(x)), 9*exp(-3*x),
+           (pi*x**3)/(1-x**2), -x**2+pi, sin(cos(x))]
+
 alfabeto = list(string.ascii_lowercase)
 num_exercicio = 1
 i_alfabet = 0
+
+# Funções que precisam limitar o Eixo Y para ficarem mais visíveis
+logaritimos = [19, 26, 27, 29]
+trigonometricas = [20, 22, 23, 24]
+outros = [39, 41, 42, 43, 44, 45, 52, 53, 58]
 
 # Para cada função dentro da lista "funcoes" ele plota os gráficos
 # e salva dentro da devida pasta.
 for i in range(len(funcoes)):
     plt.grid(True)
     plt.plot(x, funcoes[i])
-    if (i == 20 or i == 22 or i == 24):
+
+    # Limitando o Eixo Y dos logarítimos
+    if (i in logaritimos):
         plt.ylim(-4, 4)
-    elif (i == 19):
-        plt.ylim(-5, 5)
-    elif (i == 23):
+
+    # Limitando o Eixo Y das funções trigonométricas
+    elif (i in trigonometricas):
         plt.ylim(-6, 6)
+
+    elif (i in outros):
+        plt.ylim(-12, 12)
+
+    elif (i == 51):
+        plt.ylim(0, 3)
+        plt.xlim(0, 1)
+
+    elif (i == 57):
+        plt.ylim(0, 40)
+        plt.xlim(-0.5, 2)
+
     plt.xlabel("X")
     plt.ylabel("Y")
 
